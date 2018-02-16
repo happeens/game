ac0614ed3aeb69a2b0818b86d93196d89fbfefb8
Hilmar Wiegand <me@hwgnd.de>
2018-02-16 20:49:06 +0100

    :hammer: Clean up drawing

--- src/render_group.cpp
+++ src/render_group.cpp
@@ -36,29 +36,28 @@ RenderGroup::~RenderGroup() {}
 
 void RenderGroup::draw() const {
     this->shader->use();
+    this->shader->set_uniform("projection", this->projection);
+
     glBindVertexArray(this->vao);
-
-    auto rect_count = this->rect_count;
-    auto rects = &this->rects;
-
-    f32 vertices[MAX_RECT_COUNT * 8] = {};
-    GLuint elements[MAX_RECT_COUNT * 6] = {};
 
     // TODO: keep this in a buffer to avoid reallocation every
     // frame?
-    for (u32 i = 0; i < rect_count; i++) {
+    f32 vertices[MAX_RECT_COUNT * 8] = {};
+    GLuint elements[MAX_RECT_COUNT * 6] = {};
+
+    for (u32 i = 0; i < this->rect_count; i++) {
         auto vert_index = i * 8;
-        vertices[vert_index] = (f32) rects[i]->x_min;
-        vertices[vert_index + 1] = (f32) rects[i]->y_min;
+        vertices[vert_index] = (f32) this->rects[i].x_min;
+        vertices[vert_index + 1] = (f32) this->rects[i].y_min;
 
-        vertices[vert_index + 2] = (f32) rects[i]->x_max;
-        vertices[vert_index + 3] = (f32) rects[i]->y_min;
+        vertices[vert_index + 2] = (f32) this->rects[i].x_max;
+        vertices[vert_index + 3] = (f32) this->rects[i].y_min;
 
-        vertices[vert_index + 4] = (f32) rects[i]->x_min;
-        vertices[vert_index + 5] = (f32) rects[i]->y_max;
+        vertices[vert_index + 4] = (f32) this->rects[i].x_min;
+        vertices[vert_index + 5] = (f32) this->rects[i].y_max;
 
-        vertices[vert_index + 6] = (f32) rects[i]->x_max;
-        vertices[vert_index + 7] = (f32) rects[i]->y_max;
+        vertices[vert_index + 6] = (f32) this->rects[i].x_max;
+        vertices[vert_index + 7] = (f32) this->rects[i].y_max;
 
         auto elem_index = i * 6;
         auto elem = i * 4;
